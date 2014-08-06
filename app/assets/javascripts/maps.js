504c2480368aac8acf55dc3687cfaa4e3d130bd3
lrakhman <lana.rakhman@gmail.com>
2014-08-06 13:32:55 -0500

    trying to delete hardcoded data

--- app/assets/javascripts/maps.js
+++ app/assets/javascripts/maps.js
@@ -6,16 +6,18 @@ function locateUser(map) {
       map.setView([latitude, longitude], 15);
       var data = {latitude: latitude, longitude: longitude, date: getDateRange()};
       updatePage(data, 'your current location');
-    }, function () {
-      map.setView([41.8893288, -87.63722539999999], 15);
-      var data = {latitude: 41.8893288, longitude: -87.63722539999999};
-      updatePage(data, 'Dev Bootcamp');
-    });
-  } else {
-    map.setView([41.8893288, -87.63722539999999], 15);
-    var data = {latitude: 41.8893288, longitude: -87.63722539999999};
-    updatePage(data, 'Dev Bootcamp');
-  }
+    // }, function () {
+    //   map.setView([41.8893288, -87.63722539999999], 15);
+    //   var data = {latitude: 41.8893288, longitude: -87.63722539999999};
+    //   updatePage(data, 'Dev Bootcamp');
+    // });
+  } 
+  // else {
+  //   map.setView([41.8893288, -87.63722539999999], 15);
+  //   var data = {latitude: 41.8893288, longitude: -87.63722539999999};
+  //   updatePage(data, 'Dev Bootcamp');
+  // }
+}
 }
 
 function addLegend(active_map) {
